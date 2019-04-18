254d3ec97ba07f6b600335051b7fbdb474660c50
Farhan Ar Rafi <farhanarrafi@gmail.com>
2019-04-18 12:06:45 +0600

    [Android_ToolBox_Clock] Updated Clocks
    - Added Icons
    
    Signed-off-by: Farhan Ar Rafi <farhanarrafi@gmail.com>

---
+++ README.md
@@ -0,0 +1,3 @@
+
+### Icon generated via:
+https://romannurik.github.io/AndroidAssetStudio/icons-launcher.html#foreground.type=clipart&foreground.clipart=access_time&foreground.space.trim=1&foreground.space.pad=0.25&foreColor=rgba(61%2C%2084%2C%2095%2C%200)&backColor=rgb(218%2C%20226%2C%20220)&crop=0&backgroundShape=square&effects=shadow&name=ic_launcher
--- app/src/main/java/com/farhanarrafi/app/clock/MainActivity.java
+++ app/src/main/java/com/farhanarrafi/app/clock/MainActivity.java
@@ -3,7 +3,6 @@ package com.farhanarrafi.app.clock;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
-import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,6 +11,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.AnalogClock;
 import android.widget.DigitalClock;
 import android.widget.TextClock;
 import android.widget.TextView;
@@ -20,37 +20,46 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
 
     private static final String TAG = "MainActivity";
     private TextView digitalClock;
+    private AnalogClock analogClock;
     private ViewGroup rootLayout;
     private GestureDetector detector;
+    private boolean isAnalog = false;
+    private boolean isBlack = false;
+    private static final String LAST_CLOCK_TYPE = "LAST_CLOCK_TYPE";
+    private static final String LAST_BACKGROUND_COLOR = "LAST_BACKGROUND_COLOR";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if(savedInstanceState != null) {
+            isAnalog = savedInstanceState.getBoolean(LAST_CLOCK_TYPE);
+            isBlack = savedInstanceState.getBoolean(LAST_BACKGROUND_COLOR);
+        }
+
         hideActionBar();
         setContentView(R.layout.activity_main);
         rootLayout = findViewById(R.id.rootLayout);
         rootLayout.setBackgroundColor(Color.WHITE);
+
+
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
             digitalClock = (TextClock)findViewById(R.id.digitalClock);
         } else {
             digitalClock = (DigitalClock)findViewById(R.id.digitalClock);
         }
-        detector = new GestureDetector(this, new OnSwipeListener(){
-            @Override
-            public boolean onSwipe(Direction direction) {
-                if(direction == Direction.UP) {
-                    toggleBackgroundAndFont();
-                } else if (direction == Direction.DOWN) {
-                    toggleBackgroundAndFont();
-                } else if (direction == Direction.LEFT) {
+        analogClock = findViewById(R.id.analogClock);
 
-                } else if (direction == Direction.RIGHT) {
 
-                }
-                return true;
-            }
-        });
+        OnSwipeHandler handler = new OnSwipeHandler();
+        detector = new GestureDetector(this, handler);
         rootLayout.setOnTouchListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+
+        super.onResume();
     }
 
     private void hideActionBar() {
@@ -70,28 +79,6 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         }
     }
 
-
-    /**
-     * https://stackoverflow.com/a/26387629/3148856
-     * @param event
-     * @return
-     */
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-
-        int action = MotionEventCompat.getActionMasked(event);
-        switch (action) {
-            case MotionEvent.ACTION_UP:
-                //toggleBackgroundAndFont();
-                return true;
-            case MotionEvent.ACTION_DOWN:
-                toggleBackgroundAndFont();
-                return true;
-            default:
-                return super.onTouchEvent(event);
-        }
-    }
-
     private void toggleBackgroundAndFont() {
         ColorDrawable colorDrawable = (ColorDrawable) rootLayout.getBackground();
         if(colorDrawable.getColor() == Color.WHITE) {
@@ -107,11 +94,64 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         }
     }
 
+    private void toggleAnalogAndDigital() {
+        if(digitalClock.getVisibility() == View.VISIBLE) {
+            digitalClock.setVisibility(View.GONE);
+            analogClock.setVisibility(View.VISIBLE);
+            isAnalog = true;
+        } else {
+            digitalClock.setVisibility(View.VISIBLE);
+            analogClock.setVisibility(View.GONE);
+            isAnalog = false;
+        }
+    }
+
     @Override
     public boolean onTouch(View v, MotionEvent event) {
         detector.onTouchEvent(event);
         return true;
     }
 
+    private class OnSwipeHandler extends OnSwipeListener {
+        @Override
+        public boolean onSwipe(Direction direction) {
+            if(direction == Direction.UP) {
+                toggleBackgroundAndFont();
+            } else if (direction == Direction.DOWN) {
+                toggleBackgroundAndFont();
+            } else if (direction == Direction.LEFT) {
+                toggleAnalogAndDigital();
+            } else if (direction == Direction.RIGHT) {
+                toggleAnalogAndDigital();
+            }
+            return true;
+        }
+    }
 
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        isAnalog = savedInstanceState.getBoolean(LAST_CLOCK_TYPE);
+        isBlack = savedInstanceState.getBoolean(LAST_BACKGROUND_COLOR);
+        if(isAnalog) {
+            digitalClock.setVisibility(View.GONE);
+            analogClock.setVisibility(View.VISIBLE);
+        } else {
+            digitalClock.setVisibility(View.VISIBLE);
+            analogClock.setVisibility(View.GONE);
+        }
+        if(isBlack) {
+            rootLayout.setBackgroundColor(Color.BLACK);
+            digitalClock.setTextColor(Color.WHITE);
+        } else {
+            rootLayout.setBackgroundColor(Color.WHITE);
+            digitalClock.setTextColor(Color.BLACK);
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putBoolean(LAST_CLOCK_TYPE, isAnalog);
+        outState.putBoolean(LAST_BACKGROUND_COLOR, isBlack);
+        super.onSaveInstanceState(outState);
+    }
 }
